91c043d913a1228e646186a2c76f804e671dfbac
wwkler <winner23456@naver.com>
2023-12-23 11:49:31 +0900

    Feat: 🎨 'signUpBtn' Click try to BackEnd But Server 500 Error

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,6 @@
+// 전역적으로 관리되는 변수
+var interval;   // Timer를 시작하고 종료하는데 큰 공헌을 하는 변수
+
 // '중복 확인'을 click 했을 떄 '중복 확인'을 확인하는 함수 
 function checkDuplicateID() {
    // 1. setting
@@ -29,6 +32,8 @@ function checkDuplicateID() {
                 // 3. 사용자가 입력한 아이디가 '중복 확인' 됐는지 여부에 따라 서로 다른 로직 구현 
                 const is_available = response.data.is_available;
                 if (is_available) {  // 사용 가능할 때 처리할 이벤트 (아이디 중복 X)
+                  alert('아이디 설정 완료!!');
+                
                   // 3-1. '중복 확인' 버튼  숨기기(Hidden)
                   const duplicate_btn = document.getElementById('check-duplicate');
                   duplicate_btn.style.display = 'none';
@@ -96,7 +101,7 @@ function requestVerificationCode(){
     // 1. setting 
     var regMail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;   // 이메일 유효성 검사를 위한 변수 
     var input_email=document.getElementById('input_email').value;                                        // 사용자가 입력한 아이디를 가져온다.
-    const requestVerifyCode_URL=`http://localhost:8000/utils/sendCertificationNumber/`;       // 백엔드와 소통할 URL
+    const requestVerifyCode_URL=`http://localhost:8000/utils/sendCertificationNumber/`;                  // 백엔드와 소통할 URL
 
     // 2. 이메일 유효성 검사
     if (regMail.test(input_email)) {       // 이메일 형식이 맞으면?
@@ -109,9 +114,12 @@ function requestVerificationCode(){
                   console.log(response);
 
                   // 4. 타이머 5분 호출 
-                  document.querySelector('.timer-container').style.display = 'block';                  // 타이머를 보이게 함
+                  document.querySelector('.timer-container').style.display = 'inline-block';           // 타이머를 보이게 함
+                  document.querySelector('#timer').style.display='inline-block';                       // 타이머를 보이게함
                   document.getElementById('input_email').disabled=true;                                // '이메일 입력' 칸 비활성화
                   document.getElementById('receiveVerificationCodeBtn').disabled=true;                 // '인증번호 받기' 칸 비활성화
+                  document.getElementById('verifyCodeInput').disabled=false;                           // '인증번호 입력' 칸 활성화
+                  document.getElementById('checkAuthNum').style.display = 'inline-block';                     // '인증번호 확인' 버튼 보이기
                                                                                       
                   timer1();  
                 })
@@ -142,7 +150,7 @@ function timer1() {
 // 타이머 작동 함수 2
 function timer2(duration, display, emailInput, verfiyCodeInput) {
   var timer = duration, minutes, seconds;
-  var interval = setInterval(function () {
+  interval = setInterval(function () {
       minutes = parseInt(timer / 60, 10);
       seconds = parseInt(timer % 60, 10);
 
@@ -152,21 +160,162 @@ function timer2(duration, display, emailInput, verfiyCodeInput) {
       display.textContent = minutes + ":" + seconds;
 
       if (--timer < 0) {
-          clearInterval(interval);
+          alert("인증 시간이 지났습니다. 다시 시도하세요");                                       // alert
 
-          alert("인증 시간이 지났습니다. 다시 시도하세요");                                       // alert           
+          clearInterval(interval);                                                              // 타이머 종료
           display.style.display = 'none';                                                       // 타이머 숨기기
+
           emailInput.value = '';                                                                // 이메일 입력 필드 초기화
-          verfiyCodeInput.value='';                                                             // '인증번호 입력' 필드 초기화
           document.getElementById('input_email').disabled = false;                              // '이메일 입력' 칸 활성화
+
+          verfiyCodeInput.value='';                                                             // '인증번호 입력' 필드 초기화
+          document.getElementById('verifyCodeInput').disabled=true;                             // '인증번호 입력' 칸 비활성화
+
           document.getElementById('receiveVerificationCodeBtn').disabled=false;                 // '인증번호 받기' 칸 활성화
+         
+          document.getElementById('checkAuthNum').style.display = 'none';                       // '인증번호 입력' 버튼 비활성화
       }                                                                                         
   }, 1000);
 }
 
+// '인증번호 확인' 버튼 click했을 떄 이를 수행하는 함수
+function checkAuthNum(){
+    // 1. setting
+    var input_email=document.getElementById('input_email');                                                                                                           // 이메일 입력칸
+    var input_email_value=document.getElementById('input_email').value;                                                                                               // 이메일 입력값
+    var receiveVerificationCodeBtn=document.getElementById('receiveVerificationCodeBtn');                                                                             // 인증번호 받기 버튼
+    var verifyCodeInput=document.getElementById('verifyCodeInput');                                                                                                   // 인증번호 입력칸
+    var verifyCodeInputValue=document.getElementById('verifyCodeInput').value;                                                                                        // 인증번호 입력값
+    var checkAuthNumBtn=document.getElementById('checkAuthNum');                                                                                                      // 인증번호 확인 버튼
+    var timer = document.querySelector('#timer');                                                                                                                     // 타이머
+    var signUpBtn = document.getElementById('signUpBtn');                                                                                                             // 가입하기 버튼 
+
+    const checkAuthNum_URL=`http://localhost:8000/utils/checkCertificationNumber/?email=${input_email_value}&certification_number=${verifyCodeInputValue}&purpose=signup`;  // 백엔드 통신 URL
+
+    // 2. 백엔드가 마련해둔 '인증번호 확인' 코드와 연계
+    const result = function() {                     
+      axios.get(checkAuthNum_URL).then(
+        (response) => {
+          console.log(response);
+          const is_success=response.data.success;
+    
+          // 3. success 속성에 따라 서로 다른 로직 부여
+          if(is_success==true){
+            alert('인증 확인이 성공하였습니다. 회원가입 버튼이 활성화 되었습니다.');                // alert
+                                                                                                 
+            clearInterval(interval);                                                             // 타이머 중지
+            document.querySelector('.timer-container').style.display = 'none';                   // 타이머를 안보이게 한다.
+            timer.style.display='none';                                                          // 타이머 중지      
+             
+            input_email.disabled=true;                                                           // 이메일 입력칸 비활성화
+            
+            receiveVerificationCodeBtn.disabled=true;                                            // 인증번호 받기 버튼 비활성화
+
+            verifyCodeInput.disabled=true;                                                       // 인증번호 입력칸 비활성화 
+
+            checkAuthNumBtn.style.display='none';                                                // 인증번호 확인 버튼 Hidden
+
+            signUpBtn.disabled=false;                                                            // 가입하기 버튼 활성화
+          }
+          else{  
+              alert("인증확인이 실패했습니다. 다시 시도하세요");                                      // alert     
+              
+              clearInterval(interval);                                                             // 타이머 중지
+              document.querySelector('.timer-container').style.display = 'none';                   // 타이머를 안보이게 한다.
+              timer.style.display='none';                                                          // 타이머 중지
+
+              input_email.disabled=false;                                                          // 이메일 입력칸 활성화
+              input_email.value='';                                                                // 이메일 입력값 지우기
+
+              receiveVerificationCodeBtn.disabled=false;                                           // 인증번호 받기 버튼 활성화
+             
+              verifyCodeInput.disabled=true;                                                       // 인증번호 입력칸 비활성화
+              verifyCodeInput.value='';                                                            // 인증번호 입력값 지우기
+
+              checkAuthNumBtn.style.display='none';                                                // 인증번호 확인 버튼 Hidden
+          }
+      });
+    }
+
+    result();
+  
+}
+
+// 회사 선택시 1, 2, 3를 return 하는 함수 
+function getSelectedCompanyValue() {
+  var selectElement = document.getElementsByName('dropdown')[0];
+  var selectedValue = selectElement.value;
+
+  switch (selectedValue) {
+      case 'option1':
+          return 1;  // KT 선택시
+      case 'option2':
+          return 2;  // LG 선택시
+      case 'option3':
+          return 3;  // SK 선택시
+      default:
+          return 0;  // 기본값 혹은 선택되지 않았을 때
+  }
+}
+
+
 // '가입하기' 버튼 click했을 떄 이를 수행하는 함수 
 function signUp(){
+    // 1. setting
+    var input_id=document.getElementById('input_id');                                     // 아이디 입력칸    
+    var password=document.getElementById('password').value;                               // 비밀번호 value
+    var confirmPassword=document.getElementById('confirm_password').value;                // 확인 비밀번호 value
+    var name=document.getElementById('name').value;                                       // 이름 value
+    var input_email=document.getElementById('input_email');                               // 이메일 입력칸  
+    var input_verifyCode=document.getElementById('verifyCodeInput');                      // 인증번호 입력칸
+
+    var signup_URL=`http://localhost:8000/account/signUp/`;                               // 백엔드 '회원가입' URL
+
+    // 2. 유효성 검사
+    if(input_id.disabled==false){                                                          // 아이디 입력칸이 활성화 됐을 떄 
+       alert('회원 가입 실패 - 아이디 입력칸 활성화 때문');
+    } 
+    else if(password==''){                                                                 // 비밀번호가 빈칸일 떄 
+      alert('회원가입 실패 - 비밀번호 빈칸 떄문');
+    }
+    else if(password!=confirmPassword) {                                                   // 비밀번호와 확인 비밀번호가 다를 떄 
+       alert('회원가입 실패 - 비밀번호와 확인 비밀번호가 다르기 떄문');
+    }
+    else if(name==''){                                                                     // 이름을 입력하지 않았을 떄 
+       alert('회원가입 실패 - 이름을 입력하지 않았기 떄문');
+    }
+    else {
+      // console.log('user_id : ' + input_id.value);
+      // console.log('password : ' + password);
+      // console.log('user_email : ' + input_email.value);
+      // console.log('certification_number : ' + input_verifyCode.value);
+      // console.log('user_name : ' + name);
+      // console.log('company_id : ' + getSelectedCompanyValue());
+
+
   
-  
+       // 3. 백엔드와 마련한 '회원가입' 코드와 연계한다.
+       const result=function(){
+          axios.post(signup_URL, 
+                    {'user_id':input_id.value,
+                     'password':password,
+                     'user_email':input_email.value,
+                     'certification_number':input_verifyCode.value,
+                     'user_name':name,
+                     'company_id':getSelectedCompanyValue(),
+                     'privacy_agreement':true,
+                    })
+                .then(function (response) {
+                  console.log(response);
+                })
+                .catch(function (error) {
+                  console.log('에러');
+                  console.log(error);
+                });
+        }
+
+        result();  // result 함수 호출 
+          
+    }
 }
 
