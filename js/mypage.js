17bf88da1590eb53c11a7c360b52365e6f2618e0
wwkler <winner23456@naver.com>
2023-12-24 16:15:12 +0900

    📰 Add makePromptTemplate.html, makePromptTemplate.css, makePromptTemplate js files

--- js/mypage.js
+++ js/mypage.js
@@ -3,6 +3,11 @@ function routeToEditMyInfo() {
     window.location.href = '../editMyInfo.html';
 }
 
+// '프롬프트 템플릿 생성하기' 버튼을 click 했을 떄 호출되는 함수
+function makePrompotTemplate(){
+    window.location.href = '../makePromptTemplate.html';
+}
+
 // '탈퇴하기' 버튼을 click 했을 떄 호출되는 함수
 function withdrawAccount(){
     window.location.href = '../withdrawAccount.html';
