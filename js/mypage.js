be547ae735a21949c8626f9f47d22fcada1e42cd
wwkler <winner23456@naver.com>
2023-12-24 14:09:03 +0900

    📰 Add withdrawAccount.html, withdrawAccount.css, withdrawAccount js files

--- js/mypage.js
+++ js/mypage.js
@@ -1,3 +1,9 @@
+// '회원 정보 수정하기' 버튼을 click 했을 떄 호출되는 함수 
 function routeToEditMyInfo() {
     window.location.href = '../editMyInfo.html';
+}
+
+// '탈퇴하기' 버튼을 click 했을 떄 호출되는 함수
+function withdrawAccount(){
+    window.location.href = '../withdrawAccount.html';
 }
