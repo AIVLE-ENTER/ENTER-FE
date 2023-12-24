1982c34b055884208b12f6d9e5ae5e0d7691b1c4
wwkler <winner23456@naver.com>
2023-12-24 19:04:42 +0900

    📰 Add findID.html, findID.css, findID.js files

--- js/mypage.js
+++ js/mypage.js
@@ -3,6 +3,16 @@ function routeToEditMyInfo() {
     window.location.href = '../editMyInfo.html';
 }
 
+// '아이디 찾기' 버튼을 click했을 떄 호출되는 함수 
+function findID(){
+    window.location.href='../findID.html';
+}
+
+// '비밀번호 변경' 버튼을 click 했을 떄 호출되는 함수 
+function changePassword(){
+    
+}
+
 // '프롬프트 템플릿 생성하기' 버튼을 click 했을 떄 호출되는 함수
 function makePrompotTemplate(){
     window.location.href = '../makePromptTemplate.html';
