5a7660e2b6173a8e55b02b05a1b407f284eef954
wwkler <winner23456@naver.com>
2023-12-26 21:39:51 +0900

    Design: 💄 Overall UI improvements and enhancements to the bulletin 'prompt_version2'

--- js/prompt_version2.js
+++ js/prompt_version2.js
@@ -63,6 +63,46 @@ show_user_menu.addEventListener( "click", function() {
 //     show_view( ".new-chat-view" );
 // });
 
+/* 주제(Target) 목록 중에 하나의 주제를 선택했을 떄 */
+  
+const chatRoomsData = {                                                 // 각 주제별 채팅방 목록 예시 데이터
+    'gigagenie': ['기가지니 채팅방 1', '기가지니 채팅방 2', '기가지니 채팅방 3', '기가지니 채팅방 4',
+                  '기자지니 채팅방 5', '기가지니 채팅방 6', '기가지니 채팅방 7', '기가지니 채팅방 8'],
+    'pass': ['PASS 채팅방 1', 'PASS 채팅방 2'],
+    'other1': ['Other1 채팅방 1', 'Other1 채팅방 2'],
+    'other2': ['Other2 채팅방 1', 'Other2 채팅방 2'],
+    'other3': ['Other3 채팅방 1', 'Other3 채팅방 2'],
+    'other4': ['Other4 채팅방 1', 'Other4 채팅방 2'],
+    // 여기에 추가 주제별 채팅방 목록 추가 가능
+};
+
+    // 주제(Target)를 선택했으면 그에 따른 채팅방 목록도 보여주기 
+function showChats(topicKey) {
+    const chatListDiv = document.getElementById('chat-list');
+    chatListDiv.innerHTML = ''; // 기존 내용 초기화
+
+    const ul = document.createElement('ul');
+    ul.className = 'conversations';
+
+    const chats = chatRoomsData[topicKey];
+    chats.forEach(chat => {
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.textContent = chat;
+        button.onclick = function() {
+            // 여기에 채팅방 선택 시 수행할 동작 추가
+            
+            console.log(chat + ' 채팅방이 선택되었습니다.');
+            displayChatHistory(chat);  // 목록을 가져옴 
+        };
+
+        li.appendChild(button);
+        ul.appendChild(li);
+    });
+
+    chatListDiv.appendChild(ul);
+}
+
 
 /* 주제에 따른 채팅방을 click 했을 떄 과거의 히스토리를 목록으로 보여주는 함수 */
 const chatRoomHistories = {
@@ -111,48 +151,13 @@ function displayChatHistory(chatRoomKey) {
 
         conversationView.appendChild(messageDiv);
     });
+    
+    // 버튼 입력창 보여주기 
+    const message_form = document.querySelector('#message-form');
+    message_form.style.display='block';
 }
 
 
-/* 주제(Target) 목록 중에 하나의 주제를 선택했을 떄 */
-  
-const chatRoomsData = {                                                 // 각 주제별 채팅방 목록 예시 데이터
-    'gigagenie': ['기가지니 채팅방 1', '기가지니 채팅방 2', '기가지니 채팅방 3', '기가지니 채팅방 4',
-                  '기자지니 채팅방 5', '기가지니 채팅방 6', '기가지니 채팅방 7', '기가지니 채팅방 8'],
-    'pass': ['PASS 채팅방 1', 'PASS 채팅방 2'],
-    'other1': ['Other1 채팅방 1', 'Other1 채팅방 2'],
-    'other2': ['Other2 채팅방 1', 'Other2 채팅방 2'],
-    'other3': ['Other3 채팅방 1', 'Other3 채팅방 2'],
-    'other4': ['Other4 채팅방 1', 'Other4 채팅방 2'],
-    // 여기에 추가 주제별 채팅방 목록 추가 가능
-};
-
-    // 주제(Target)를 선택했으면 그에 따른 채팅방 목록도 보여주기 
-function showChats(topicKey) {
-    const chatListDiv = document.getElementById('chat-list');
-    chatListDiv.innerHTML = ''; // 기존 내용 초기화
-
-    const ul = document.createElement('ul');
-    ul.className = 'conversations';
-
-    const chats = chatRoomsData[topicKey];
-    chats.forEach(chat => {
-        const li = document.createElement('li');
-        const button = document.createElement('button');
-        button.textContent = chat;
-        button.onclick = function() {
-            // 여기에 채팅방 선택 시 수행할 동작 추가
-            
-            console.log(chat + ' 채팅방이 선택되었습니다.');
-            displayChatHistory(chat);  // 목록을 가져옴 
-        };
-
-        li.appendChild(button);
-        ul.appendChild(li);
-    });
-
-    chatListDiv.appendChild(ul);
-}
 
 
 // 'New Chat'click 시 팝업 나타나는 함수 
@@ -173,6 +178,10 @@ function useWell() {
 // 팝업 닫기 함수 
 function closeWellPhrasesPopup() {
     document.getElementById('wellPhrasesPopup').style.display = 'none';
+}
+
+function exit(){
+    window.location.href = '../index.html'; // 홈페이지 URL로 변경하세요
 }
 
 
