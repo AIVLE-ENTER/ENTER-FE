2164c64887ef3223edae39101eaeb1a774392e32
Rumid98 <bout123456@naver.com>
2023-12-27 16:01:01 +0900

    🎨 inquiry onclick added

--- js/inquiryBoard.js
+++ js/inquiryBoard.js
@@ -1,20 +1,43 @@
-$.ajax({
-    url: '/board/',
-    type: 'GET',
-    dataType: 'json',
-    success: function(data) {
-        console.log('-------------------------json')
-        console.log(data)
-        data.forEach(function(board) {
-            $('boardList').append('<li><strong>' + board.title + '</strong>: ' + board.content + '</li>');
+//import {detailRendering} from "./inquiryDetail.js"
+
+
+var contentNumber = 1;
+
+tempUrl = 'http://127.0.0.1:8000/board/'
+axios.get(tempUrl)
+    .then((response) => {
+        console.log('success');
+        console.log(typeof(response));
+        const board_list = response.data['post_list']
+        console.log(board_list)
+
+        board_list.forEach((content) => {
+            console.log(content)
+            
+            htmlItem = document.createElement('tbody');
+            htmlItem.onclick = function() {
+                redirectToDetailPage(content.board_id);
+            }
+            htmlItem.innerHTML = `<td>${contentNumber++}</td>
+                                  <td>${content.question_type_title}</td>
+                                  <td>${content.question_title}</td>
+                                  <td>${content.user_name}</td>
+                                  <td>${content.question_datetime.substr(0, 10)}</td>
+                                  <td>${15}</td>`
+            document.getElementById('boardList').appendChild(htmlItem);
+            
         });
-    },
-    error: function(error) {
-        console.log(error);
-    }
-});
+    })
+    .catch((error) => {
+        console.log(`error: ${error}`);
+    })
+    .finally(() => {
+        console.log('End.')
+    })
+
 
 function redirectToDetailPage(postId) {
+    detailRendering(postId);
     window.location.href = '/inquiryDetail.html?id=' + postId;
 }
 
---
+++ js/inquiryDetail.js
@@ -0,0 +1,40 @@
+
+
+function detailRendering(postId) {
+    tempUrl = 'http://127.0.0.1:8000/board/' + posdId
+
+    axios.get(tempUrl)
+    .then((response) => {
+        console.log('success');
+        console.log(typeof(response));
+        const board_list = response.data['post_list']
+        console.log(board_list)
+
+        board_list.forEach((content) => {
+            console.log(content)
+            
+            htmlItem = document.createElement('tbody');
+            htmlItem.onclick = function() {
+                redirectToDetailPage(content.board_id);
+            }
+            htmlItem.innerHTML = `<td>${contentNumber++}</td>
+                                  <td>${content.question_type_title}</td>
+                                  <td>${content.question_title}</td>
+                                  <td>${content.user_name}</td>
+                                  <td>${content.question_datetime.substr(0, 10)}</td>
+                                  <td>${15}</td>`
+            document.getElementById('boardList').appendChild(htmlItem);
+            
+        });
+    })
+    .catch((error) => {
+        console.log(`error: ${error}`);
+    })
+    .finally(() => {
+        console.log('End.')
+    })
+}
+
+export {detailRendering};
+
+
