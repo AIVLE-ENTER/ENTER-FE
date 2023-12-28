858525fb5329e99f92f2840be3ff75a59df7eb3b
Rumid98 <bout123456@naver.com>
2023-12-28 14:23:00 +0900

    🎨 mypage design modified

--- js/inquiryDetail.js
+++ js/inquiryDetail.js
@@ -18,23 +18,6 @@ axios.get(tempUrl)
     faqItemTitle.append(inquiryData['question_title'])
     faqItemWriter.append(inquiryData['user_name'])
     faqItemContent.append(inquiryData['question_content'])
-
-    // board_list.forEach((content) => {
-    //     console.log(content)
-        
-    //     htmlItem = document.createElement('tbody');
-    //     htmlItem.onclick = function() {
-    //         redirectToDetailPage(content.board_id);
-    //     }
-    //     htmlItem.innerHTML = `<td>${contentNumber++}</td>
-    //                           <td>${content.question_type_title}</td>
-    //                           <td>${content.question_title}</td>
-    //                           <td>${content.user_name}</td>
-    //                           <td>${content.question_datetime.substr(0, 10)}</td>
-    //                           <td>${15}</td>`
-    //     document.getElementById('boardList').appendChild(htmlItem);
-        
-    // });
 })
 .catch((error) => {
     console.log(`error: ${error}`);
