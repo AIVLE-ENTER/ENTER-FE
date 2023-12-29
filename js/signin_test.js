fe07001e1ce2bbc88a684641ed3be315bc3c82af
wwkler <winner23456@naver.com>
2023-12-29 15:59:56 +0900

    Refactor: 🔨 show UserChatList in index_test.html

--- js/signin_test.js
+++ js/signin_test.js
@@ -10,15 +10,10 @@ function signin() {
         // localStroage에 토큰(Token)을 적재한다.
         const accessToken = response['data']['data']['token'];
         localStorage.setItem('accessToken', accessToken);
-<<<<<<< HEAD
-        window.location.href = 'index_test.html';
-
-=======
         
         // index_test.html로 Routing 한다.
         alert('로그인 완료!');
         window.location.href = '../index_test.html';
->>>>>>> 4bb20a6bb0d05a97f25e1928f4f94da9c63c7ebc
     })
     .catch(error => {
         console.log(error);
