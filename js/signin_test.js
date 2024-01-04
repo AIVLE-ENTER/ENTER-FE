01e33e09759254df2a8dfa53d1dd99017fec1659
user <byhoon3@icloud.com>
2024-01-04 11:59:12 +0900

    FEAT: ✨ add google login

--- js/signin_test.js
+++ js/signin_test.js
@@ -36,8 +36,8 @@ function signin() {
 
 // '로그인' button mouseOver 했을 떄 실행되는 함수
 function changeButtonStyle(button) {
-    button.style.boxShadow = '5px 5px 10px rgba(0, 0, 0, 0.5)'; // 마우스 오버 시 그림자 추가
-  }
+    button.style.boxShadow = '3px 3px 8px rgba(0, 0, 0, 0.5)'; // 마우스 오버 시 그림자 추가
+}
   
 // '로그인' button  mouseOut 했을 떄 실행되는 함수 
 function resetButtonStyle(button) {
@@ -119,7 +119,39 @@ const kakaoLogin = async () => {
 
 // 네이버 로그인
 
+
 // 구글 로그인
+const googleLogin = async() => {
+    axios({
+        url: 'http://localhost:8000/account/googleLogin/',
+        method: 'post',
+        data: {code: code},
+    })
+    .then(response => {
+        console.log('성공:', response.data); // 로그에 응답 데이터를 찍습니다.
+
+        // 2-1. 로그인 성공 (로그인 후 프롬프트 페이지로 이동)
+        if (response.data.message == "exists") {
+            console.log("로그인 성공");
+            // localStroage에 토큰(Token)을 적재한다.
+            const accessToken = response.data.data.token;
+            setWithExpire('accessToken', accessToken, 12*60*60*1000); //12 시간
+
+            // index_test.html로 Routing 한다.
+            alert('로그인 완료!');
+            window.location.href = '../index_test.html';
+        } else {
+            // 2-2. 로그인 실패 (모달창 띄우기)
+            modal.style.display = 'block';
+            document.getElementById("socialType").value = "google";
+            document.getElementById("socialId").value = response.data.data.id;
+            document.getElementById("signUpBtn").setAttribute("onclick", `location.href='${signUpURL}?type=google&socialId=${response.data.data.id}'`);
+        }
+    })
+    .catch(error => {
+        console.log('실패', error);
+    });
+}
 
 
 // code가 있으면?
@@ -129,7 +161,7 @@ if (code) {
     } else if (type == 'naver') {
         console.log("네이버");
     } else if (type == 'google') {
-        console.log('구글');
+        googleLogin();
     }
 } 
     
